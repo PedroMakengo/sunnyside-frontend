d36225206ec6e18f42ea48c6dae0f67c2498d0c6
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-08-18 14:45:11 -0300

    Algumas alterações

--- scripts/sunnyside.js
+++ scripts/sunnyside.js
@@ -1,3 +1,9 @@
 let btn = document.getElementById("btn");
+let menu = document.getElementById("menu");
 
-btn.addEventListener("click", function (e) {});
+btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  let x = menu.toggleAttribute("active");
+  console.log(x);
+});
